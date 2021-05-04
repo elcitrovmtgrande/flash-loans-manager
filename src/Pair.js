c9872456e63b54b81cc6ed4f923824156d48389a
elcitrovmtgrande <victor.mahe.pro@gmail.com>
2021-05-04 15:29:08 +0200

    Adding data API creds

--- src/Pair.js
+++ src/Pair.js
@@ -2,6 +2,18 @@ class Pair {
   constructor(pair) {
     this.pair = pair;
   }
+
+  async watch() {
+    // todo
+    /**
+     * 1) Je recupere l'id block ETH de la blockchain ethereum
+     * 2) Je recupere le contenu du block grace a son id
+     * 3) Je recupere les transactions
+     * 4) Je recherche les valeurs pour ma paire
+     * 5) Je calcule une moyenne pour ces valeurs s'il y en a plusieurs
+     * 6) J'execute la callback en cas d'erreur d'arbitrage
+     */
+  }
 }
 
 export default Pair;
--- src/config.js
+++ src/config.js
@@ -1,4 +1,19 @@
 const PORT = 3000;
+const INFURA_CREDENTIALS = {
+  projectName: 'flash-loans-manager',
+  projectId: '1s4WhA9I1fDPz5qPebhcQUUyMAR',
+  projectSecret: 'ae20c05a38a8593870244f8749bfa931',
+  endpoints: {
+    https: 'https://1s4WhA9I1fDPz5qPebhcQUUyMAR:ae20c05a38a8593870244f8749bfa931@eth2-beacon-mainnet.infura.io',
+    socket: 'wss://1s4WhA9I1fDPz5qPebhcQUUyMAR:ae20c05a38a8593870244f8749bfa931@eth2-beacon-mainnet.infura.io',
+  },
+};
 
-export { PORT };
-export default { PORT };
+const ALCHEMY_CREDENTIALS = {
+  endpoints: {
+    https: 'https://eth-mainnet.alchemyapi.io/v2/caTQ647EOB-SR1MLFlRpT5cZZ1ixMG45',
+  },
+};
+
+export { PORT, INFURA_CREDENTIALS, ALCHEMY_CREDENTIALS };
+export default { PORT, INFURA_CREDENTIALS, ALCHEMY_CREDENTIALS };
