84d5a55c83641dc462f5bb77c329577a638cd9e4
elcitrovmtgrande <victor.mahe.pro@gmail.com>
2021-05-10 21:14:19 +0200

    Amelioration minime du front

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import { CRYPTOS } from './config';
 import { StrategyManager } from './StrategyManager';
 
 const app = express();
 
+app.get('/cryptos', (req, res) => {
+  res.json(CRYPTOS);
+});
+
 app.use('/app', express.static('./src/frontend'));
+
 const http = require('http').createServer(app);
+
 const io = require('socket.io')(http);
 
 http.listen(3000, '127.0.0.1');
