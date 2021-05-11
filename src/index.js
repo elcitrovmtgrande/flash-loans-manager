e151c7f9f509b4f0fcf3c5b75d072e63dbd416e3
Baptiste Harle <baptiste.harle@gmail.com>
2021-05-11 16:56:34 +0200

    wip api point pair

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import express from 'express';
 import { CRYPTOS } from './config';
 import { StrategyManager } from './StrategyManager';
+import { Pair } from './Pair';
 
 const app = express();
 
 app.get('/api/cryptos', (req, res) => {
   res.json(CRYPTOS);
+});
+
+app.post('/api/pair', async (req, res) => {
+  const { Pair } = req.body;
+  if (Pair) {
+
+  } else {
+    res.json({ message: 'Dont forgat da pair noob' });
+  }
 });
 
 const http = require('http').createServer(app);
